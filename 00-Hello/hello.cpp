e2b37cc02ab38f92d4194e2b4e6f7281ea3483d8
LOlivastri-FRBa <lolivastri@frba.utn.edu.ar>
2026-04-29 01:38:27 -0300

    Update hello.cpp

--- 00-Hello/hello.cpp
+++ 00-Hello/hello.cpp
@@ -2,5 +2,9 @@
 
 int main() {
     std::cout << "Hola mundo" << std::endl;
+    std::cout << "Nombre y apellido: Lautaro Olivastri" << std::endl;
+    std::cout << "Curso: K1053" << std::endl;
+    std::cout << "Grupo: (tu grupo)" << std::endl;
+
     return 0;
 }
